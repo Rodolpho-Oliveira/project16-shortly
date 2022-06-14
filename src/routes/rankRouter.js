117796d689e01cb64f5d810d6b64ceac987c9574
Vinicius <vinigugelmin@hotmail.com>
2022-06-14 11:35:09 -0300

    Preparando revisão de código

--- src/routes/rankRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { showRank } from "../controllers/rankController.js"
-
-const rankRouter = Router()
-
-rankRouter.get("/ranking", showRank)
-
-export default rankRouter
